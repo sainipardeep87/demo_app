faf82567f9f7a502ab77cfa6d44588951f14e559
Pardeep Kumar <saini.pardeep87@gmail.com>
2013-03-29 10:08:17 +0530

    commit after completing sign_in sign_out

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,8 +27,14 @@ describe "User pages" do
         it "should not create user" do
           expect {click_button submit}.not_to change(User, :count)
         end
+        describe "after submission" do
+          before { click_button submit}
+          
+          it { should have_selector('title', text: 'Sign up') }
+          it { should have_content('error') }
+        end
     end
-    
+        
     describe "with valid information" do 
       before do
         fill_in "Name",         with: "Example User"
@@ -36,7 +42,7 @@ describe "User pages" do
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
-      it "should create usre" do
+      it "should create user" do
         expect { click_button submit}. to change(User, :count).by(1)
       end
     end
